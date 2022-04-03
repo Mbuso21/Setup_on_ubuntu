b207324f4906ddad0bb375174294c187b1d2b744
mmehloma <mmehloma@student.wethinkcode.co.za>
2022-04-03 10:28:24 +0200

    Added an auto yes to all the installs

--- setup_script.sh
+++ setup_script.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+# Install neofetch
+
+sudo apt install -y neofetch
+
+neofetch
+
+sleep 5
+
 # Installing git
 
-sudo ap install git
+sudo apt -y install git
 
 #Installing brave
 
@@ -11,15 +19,15 @@ echo 'Installing brave browser...please press enter '
 echo ""
 
 
-sudo apt install apt-transport-https curl
+sudo apt install -y apt-transport-https curl
 
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 
-sudo apt update
+sudo apt -y update
 
-sudo apt install brave-browser
+sudo apt install -y brave-browser
 
 echo ""
 
@@ -37,7 +45,8 @@ echo ""
 
 wget https://downloads.slack-edge.com/linux_releases/slack-desktop-4.0.2-amd64.deb
 
-sudo apt install ./slack-desktop-*.deb
+sudo apt -y install ./slack-desktop-*.deb
+
 
 echo ""
 
@@ -53,7 +62,7 @@ sudo apt install gdebi-core wget
 
 wget -O ~/discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
 
-sudo gdebi ~/discord.deb
+sudo gdebi -y ~/discord.deb
 
 echo ""
 echo ""
@@ -68,7 +77,7 @@ echo ""
 
 echo ""
 
-sudo apt install snapd
+sudo apt install -y snapd
 
 echo ""
 echo ""
@@ -82,7 +91,7 @@ echo ""
 
 echo ""
 
-sudo snap install intellij-idea-ultimate --classic
+sudo snap install -y intellij-idea-ultimate --classic
 
 echo ""
 echo ""
@@ -94,13 +103,13 @@ echo "Installing maven"
 
 echo ""
 
-sudo apt install maven
+sudo apt install -y maven
 
 echo ""
 
 # Install gradle
 
-sudo apt install gradle
+sudo apt install -y gradle
 
 # Installing tweaks
 
@@ -110,7 +119,7 @@ echo ""
 
 echo ""
 
-sudo apt install gnome-tweak-tool
+sudo apt  install -y gnome-tweak-tool
 sudo add-apt-repository universe
 
 echo ""
@@ -121,125 +130,27 @@ echo "Installing xclip"
 
 echo ""
 
-sudo apt install xclip
-
-echo ""
-
-# Setup git globals
-
-echo 'Setup git globals'
-
-echo ''
-
-read -p 'Please enter your username: ' gitGlobalUsername
-read -p 'Please enter yout email: ' gitGlobalEmail
-
-git config --global user.name $gitGlobalUsername
-git config --global user.email $gitGlobalEmail
-
-echo "Here are your git globals:"
-echo ""
-
-cat ~/.gitconfig
-
-echo ""
-echo ""
-echo ""
-
-# Setup SSH
-
-echo "Setting up SSH"
-
-echo ""
-
-
-ssh-keygen -t ed25519 -C $gitGlobalEmail
-
-echo ""
-
-xclip -sel clip < ~/.ssh/id_ed25519.pub
-
-echo "Please open https://gitlab.wethinkco.de on your browser"
-
-echo ""
-
-echo "Gitlab will open via your browser"
-echo "Login to it using your WTC gmail credentials"
+sudo apt install -y xclip
 
 echo ""
 
 echo ""
-echo "Please paste on the ADD SSH in your gitlab"
-echo ""
-echo ""
 
-
-echo ""
-
-read -p "Press enter once you've Added your SSH KEY" key
-
-ssh -T git@gitlab.wethinkco.de
-
-echo ""
-
-echo "If there is a problem, you will have to do the process manually"
-
-echo ""
-
-read -p "Press enter to proceed" enter
-
-echo ""
-
-# Generate a GPG KEY
-
-echo "Generating a GPG KEY"
-
-echo ""
-
-echo "You can follow the steps needed by opening https://gitlab.wethinkco.de/help/user/project/repository/gpg_signed_commits/index.md in your browser"
-
-echo ""
-
-echo "Are you ready to proceed?"
-
-echo ""
-
-read -p "Press ENTER to proceed" enter
-
-gpg --full-gen-key
-
-echo ""
-
-gpg --list-secret-keys --keyid-format LONG $gitGlobalEmail | grep sec
-
-echo ""
-
-read -p "Please copy your sec and paste it here: " sec
-
-echo ""
-
-gpg --armor --export $sec
-
-echo ""
-
-git config --global user.signingkey $sec
-
-read -p "Press Enter when done" enter
-
-git config --global commit.gpgsign true
-
-echo ""
 
 # Updating before reboot
 
 echo  "Updating "
 
-sudo apt update
+sudo apt -y update
 
-sudo apt upgrade
+sudo apt -y upgrade
 
 echo ""
 echo ""
+
+# Remove all .deb packages before reboot
+
+rm *.deb
 
 # Reboot
 
@@ -249,10 +160,4 @@ sleep 5
 
 echo ""
 
-reboot
-
-
-
-
-
-
+sudo reboot
